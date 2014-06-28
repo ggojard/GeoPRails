1873decd9d7d133ab002810571a73cdbcf72b484
Pouya <pouyajoon@gmail.com>
2014-06-28 15:48:50 +0200

    update migrations

--- db/migrate/20140627192621_create_organizations.rb
+++ db/migrate/20140627192621_create_organizations.rb
@@ -1,5 +1,6 @@
 class CreateOrganizations < ActiveRecord::Migration
   def change
+    drop_table :organizations
     create_table :organizations do |t|
       t.string :name
       t.references :organization_type, index: true
