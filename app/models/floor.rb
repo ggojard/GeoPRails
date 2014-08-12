edeb9d1ec33e296da2cdace387b994a23625c149
Pouya <pouyajoon@gmail.com>
2014-08-12 22:22:27 +0200

    update building nil?

--- app/models/floor.rb
+++ app/models/floor.rb
@@ -23,7 +23,7 @@ class Floor < ActiveRecord::Base
       b.(self, :name, :id, :image, :map_scale_x1, :map_scale_y1, :map_scale_x2, :map_scale_y2, :map_scale_length, :image_dimensions, :fullname)
       b.url "/floors/" + self.id.to_s
       b.rooms self.rooms.collect { |r| r.to_builder_with_affectations.attributes! }
-      if !self.building_id?
+      if !self.building_id.nil?
         @o = Building.find_by_id(self.building_id)
         b.building  @o.to_builder_simple_floor.attributes!
       else
