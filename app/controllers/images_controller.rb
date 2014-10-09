a902f16e107fae2dfd1f08d882044c956ab2dc86
Pouya <pouyajoon@gmail.com>
2014-10-09 20:26:17 +0200

    update translation + set back filters texts

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,6 +1,13 @@
 class ImagesController < ApplicationController
 
-  # caches_action :company_image
+#   caches_action :company_image, :cache_path => proc { |c|
+#     expires_in 10.minutes, :public => false
+#     category = c.params[:id]
+#     {:id => category} 
+# }
+
+  # caches_action :company_image, :if => Proc.new { |c| result = c.headers["Content-Type"] = 'image/png; charset=UTF-8' if result; result }
+
 
 
   def floor_image
@@ -40,8 +47,11 @@ class ImagesController < ApplicationController
     id = params[:id]
     @f = CompanyImages.where(company_id:id, style: @s)
     response.headers['Cache-Control'] = "public, max-age=#{12.hours.to_i}"
-    response.headers['Content-Disposition'] = 'inline'
     response.headers['Content-Type'] = 'image/png'
-    render :text => @f[0].file_contents, :content_type => 'image/png'
+    response.headers['Content-Disposition'] = 'inline'
+    # response.headers['Keep-Alive'] = 'timeout=5, max=100'
+    
+    # render :plain => 'hello', :status =>  200
+    render :text => @f[0].file_contents
   end
 end
