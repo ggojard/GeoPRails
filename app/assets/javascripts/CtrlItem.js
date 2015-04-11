a22fd29e3410c31164fd50386c41fde1ab4e2cb8
Pouya <pouyajoon@gmail.com>
2015-04-11 20:07:03 +0200

    update search items

--- app/assets/javascripts/CtrlItem.js
+++ app/assets/javascripts/CtrlItem.js
@@ -4,10 +4,17 @@
   geoP.app.controller('ItemCtrl', function($scope) {
     // debugger;
     $scope.items = gon.items;
+
+    $scope.itemFilter = function(item) {
+      return item.name.search(new RegExp($scope.query, "i")) !== -1 
+      || item.code.search(new RegExp($scope.query, "i")) !== -1
+      || item.description.search(new RegExp($scope.query, "i")) !== -1;
+    }
+
   }).filter('highlight', function($sce) {
-  return function(text, phrase) {
-    if (phrase) text = text.replace(new RegExp('('+phrase+')', 'gi'), '<span class="highlighted">$1</span>')
-    return $sce.trustAsHtml(text)
-  }
-});
+    return function(text, phrase) {
+      if (phrase) text = text.replace(new RegExp('(' + phrase + ')', 'gi'), '<span class="highlighted">$1</span>')
+      return $sce.trustAsHtml(text)
+    }
+  });
 }(GeoP));
