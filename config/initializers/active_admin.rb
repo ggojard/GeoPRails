fcfc84c448459c7b4b4094774727ecea3a3e4266
Pouya <pouyajoon@gmail.com>
2015-04-13 21:21:20 +0200

    update logo size

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -20,7 +20,7 @@ ActiveAdmin.setup do |config|
   #
   # Note: Aim for an image that's 21px high so it fits in the header.
   #
-  config.site_title_image = "/images/logo_small.png"
+  config.site_title_image = "/images/logo.png"
 
   # == Default Namespace
   #
