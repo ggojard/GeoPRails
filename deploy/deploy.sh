706832d1d4494d8df8533ba73386130acebbc5c7
Pouya <pouyajoon@gmail.com>
2015-05-18 00:12:32 +0200

    update script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,13 +4,25 @@ contains() {
   [[ $1 =~ $2 ]] && exit 0 || exit 1
 }
 
-sites="mdm demo amf acf hi bouygues volkswagen heineken"
-echo "load <$1> in <$sites>"
+sites="demo amf acf hi bouygues volkswagen heineken"
 
-if (contains "$sites" $1 -eq 0); then
-  site="surfy$1"
-  echo "deploy <$site>"
-  git push origin master && git push $site master && heroku run rake db:migrate --app $site
-else
-  echo "site <$1> not found"
-fi
+deply_site(){
+
+  echo "load <$1> in <$sites>"
+
+  if (contains "$sites" $1 -eq 0); then
+    site="surfy$1"
+    echo "deploy <$site>"
+    git push origin master && git push $site master && heroku run rake db:migrate --app $site
+  else
+    echo "site <$1> not found"
+  fi
+}
+
+for s in $1 
+do
+  deply_site "$s"
+done
+
+
+
