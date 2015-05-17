ecb57d9c1fc57de0c71a677f00da2c0a8ebe5c67
Pouya <pouyajoon@gmail.com>
2015-05-18 00:08:13 +0200

    fix the person loading page

--- app/assets/javascripts/CtrlPerson.js
+++ app/assets/javascripts/CtrlPerson.js
@@ -1,16 +1,17 @@
 /*global GeoP:true, gon:true*/
 (function(geoP) {
   'use strict';
-  geoP.app.controller('PersonCtrl', function($scope) {
+  geoP.app.controller('PersonCtrl', function($scope, $rootScope) {
     var p = gon.person;
     $scope.a = {
       person: p
     };
+    $rootScope.$emit('stop-loading');
   });
 
   geoP.app.controller('PeopleCtrl', function($scope, $rootScope) {
     $scope.personFilter = function(a) {
-      return a.person.fullname.search(new RegExp($scope.query, "i")) !== -1;
+      return a.person.fullname.search(new RegExp($scope.query, 'i')) !== -1;
     };
     $scope.people = gon.people.map(function(p) {
       return {
