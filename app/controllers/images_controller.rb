61c3365ffccae1e1b32bbfda3f5875238577d983
Pouya <pouyajoon@gmail.com>
2015-10-09 23:18:16 +0200

    set surfy logo for the moment

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -32,11 +32,12 @@ class ImagesController < GeopController
 
 
   def logo
-    company = Company.find_by_id(current_admin_user.company_id)
-    if !company.nil?
-      return redirect_to company.logo.url(:small)
-    end
     return redirect_to "/assets/logo-surfy-h42.png"
+
+    # company = Company.find_by_id(current_admin_user.company_id)
+    # if !company.nil?
+    #   return redirect_to company.logo.url(:small)
+    # end
   end
 
 end
