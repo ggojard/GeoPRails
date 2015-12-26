1779d148a2aaa861d1eb0412ddfbeb41420c5865
Pouya <pouyajoon@gmail.com>
2015-12-26 12:26:38 +0100

    reduce repo size

--- delete_from_repo.sh
+++ delete_from_repo.sh
@@ -1,7 +1,7 @@
-git filter-branch --tag-name-filter cat --index-filter 'git rm -r --cached --ignore-unmatch app/assets/images/UV_Grid_Sm.jpg' --prune-empty -f -- --all
+#git filter-branch --tag-name-filter cat --index-filter 'git rm -r --cached --ignore-unmatch' --prune-empty -f -- --all
 
 
-#rm -rf .git/refs/original/
-#git reflog expire --expire=now --all
-#git gc --prune=now
-#git gc --aggressive --prune=now
+rm -rf .git/refs/original/
+git reflog expire --expire=now --all
+git gc --prune=now
+git gc --aggressive --prune=now
