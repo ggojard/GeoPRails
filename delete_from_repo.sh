d889c04e63fe34288fcafd65c0020b4ad9e86214
Pouya <pouyajoon@gmail.com>
2015-12-26 12:35:07 +0100

    reduce repo size

--- delete_from_repo.sh
+++ delete_from_repo.sh
@@ -1,6 +1,6 @@
-git filter-branch --tag-name-filter cat --index-filter 'git rm -r --cached --ignore-unmatch .DS_Store' --prune-empty -f -- --all
+#git filter-branch --tag-name-filter cat --index-filter 'git rm -r --cached --ignore-unmatch .DS_Store' --prune-empty -f -- --all
 
-#rm -rf .git/refs/original/
-#git reflog expire --expire=now --all
-#git gc --prune=now
-#git gc --aggressive --prune=now
+rm -rf .git/refs/original/
+git reflog expire --expire=now --all
+git gc --prune=now
+git gc --aggressive --prune=now
