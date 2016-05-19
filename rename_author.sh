2746f5a1e64a0d3547caf9377a05ca52990246b4
Pouya <pouyajoon@gmail.com>
2016-05-19 21:08:12 +0200

    update name

---
+++ rename_author.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+git filter-branch -f --env-filter '
+OLD_EMAIL="Pouya.Mohtacham@Casewise.com"
+CORRECT_NAME="pouya"
+CORRECT_EMAIL="pouyajoon@gmail.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
